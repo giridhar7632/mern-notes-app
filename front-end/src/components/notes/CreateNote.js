948305bc69b310a118c2902431fc5379cae4e8ba
giridhar7632 <giridhar.talla2002@gmail.com>
2021-06-25 15:25:27 +0530

    create and delete notes

--- front-end/src/components/notes/CreateNote.js
+++ front-end/src/components/notes/CreateNote.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Button } from '@chakra-ui/button'
+import { useHistory } from 'react-router-dom'
 import {
   FormControl,
   FormLabel,
@@ -7,7 +8,9 @@ import {
   Textarea,
   Heading,
   Box,
+  useToast,
 } from '@chakra-ui/react'
+import axios from 'axios'
 
 const CreateNote = () => {
   const [note, setNote] = useState({
@@ -16,16 +19,49 @@ const CreateNote = () => {
     date: '',
   })
 
+  const history = useHistory()
+  const toast = useToast()
+  const toastIdRef = useRef()
+  const addToast = (text, type) => {
+    toastIdRef.current = toast({
+      title: `${text}`,
+      status: `${type}`,
+      isClosable: true,
+      duration: 3000,
+    })
+  }
+
   const handleChange = e => {
     const { name, value } = e.target
     setNote({ ...note, [name]: value })
+  }
+
+  const createNote = async e => {
+    console.log('create')
+    e.preventDefault()
+    try {
+      const token = localStorage.getItem('userToken')
+      if (token) {
+        const { title, content, date } = note
+        const newNote = { title, content, date }
+
+        const res = await axios.post('/api/notes', newNote, {
+          headers: { Authorization: token },
+        })
+        console.log(res)
+        addToast(res.data.msg, res.data.type)
+        return history.push('/')
+      }
+    } catch (error) {
+      window.location.href = '/'
+    }
   }
 
   return (
     <div>
       <Box w="30vw">
         <Heading my={4}> Create Note</Heading>
-        <form>
+        <form onSubmit={createNote}>
           <FormControl>
             <FormLabel>Title</FormLabel>
             <Input
